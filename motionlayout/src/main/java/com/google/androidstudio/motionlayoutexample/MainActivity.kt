cb1be6fe0c1fd157b725551a083d823868678f6a
xuyisheng <xuyisheng@yuewen.com>
2020-05-14 14:12:12 +0800

    update

--- motionlayout/src/main/java/com/google/androidstudio/motionlayoutexample/MainActivity.kt
+++ motionlayout/src/main/java/com/google/androidstudio/motionlayoutexample/MainActivity.kt
@@ -21,15 +21,18 @@ class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
     private var doShowPaths = false
 
     private val dataset: Array<DemosAdapter.Demo> = arrayOf(
-            DemosAdapter.Demo("Basic Example (1/3)", "Basic motion example using referenced ConstraintLayout files", R.layout.motion_01_basic),
-            DemosAdapter.Demo("Basic Example (2/3)", "Basic motion example using ConstraintSets defined in the MotionScene file", R.layout.motion_02_basic),
-            DemosAdapter.Demo("Basic Example (3/3)", "Basic motion example same as 2, but autoComplete is set to false in onSwipe", R.layout.motion_02_basic_autocomplete_false),
+            DemosAdapter.Demo("Basic Example (1/4)", "Basic motion example using referenced ConstraintLayout files", R.layout.motion_01_basic),
+            DemosAdapter.Demo("Basic Example (2/4)", "Basic motion example using ConstraintSets defined in the MotionScene file", R.layout.motion_02_basic),
+            DemosAdapter.Demo("Basic Example (3/4)", "Basic motion example same as 2, but autoComplete is set to false in onSwipe", R.layout.motion_02_basic_autocomplete_false),
+            DemosAdapter.Demo("Basic Example (4/4)", "Basic motion example ", R.layout.motion_29_basic),
             DemosAdapter.Demo("Custom Attribute", "Show color interpolation (custom attribute)", R.layout.motion_03_custom_attribute),
             DemosAdapter.Demo("ImageFilterView (1/2)", "Show image cross-fade (using ML's ImageFilterView + custom attribute)", R.layout.motion_04_imagefilter),
             DemosAdapter.Demo("ImageFilterView (2/2)", "Show image saturation transition (using ML's ImageFilterView + custom attribute)", R.layout.motion_05_imagefilter),
             DemosAdapter.Demo("Keyframe Position (1/3)", "Use a simple keyframe to change the interpolated motion", R.layout.motion_06_keyframe),
             DemosAdapter.Demo("Keyframe Interpolation (2/3)", "More complex keyframe, adding rotation interpolation", R.layout.motion_07_keyframe),
             DemosAdapter.Demo("Keyframe Cycle (3/3)", "Basic example of using a keyframe cycle ", R.layout.motion_08_cycle),
+            DemosAdapter.Demo("Keyframe KeyAttributes(1/2)", "KeyAttributes", R.layout.motion_27_keyframe),
+            DemosAdapter.Demo("Keyframe KeyAttributes(2/2)", "KeyAttributes", R.layout.motion_28_keyframe),
             DemosAdapter.Demo("CoordinatorLayout Example (1/3)", "Basic example of using MotionLayout instead of AppBarLayout", R.layout.motion_09_coordinatorlayout),
             DemosAdapter.Demo("CoordinatorLayout Example (2/3)", "Slightly more complex example of MotionLayout replacing AppBarLayout, with multiple elements and parallax background", R.layout.motion_10_coordinatorlayout),
             DemosAdapter.Demo("CoordinatorLayout Example (3/3)", "Another AppBarLayout replacement example", R.layout.motion_11_coordinatorlayout),
